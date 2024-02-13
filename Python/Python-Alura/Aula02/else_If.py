ca19ed0cf2e2ac76a69eee4464fe205736429ded
rafaelrvs <rafaelvieira1333@gmail.com>
2024-02-13 19:56:35 -0300

    aula 02 - if elif else

--- Python/Python-Alura/Aula02/else_If.py
+++ Python/Python-Alura/Aula02/else_If.py
@@ -1,4 +1,3 @@
-
 print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -7,18 +6,6 @@ print("""
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
 """)
-print('Sabor express \n')
-
-
-
-print('1. Cadastrar Restaurante' )
-print('2. Listar Restaurante' )
-print('3. Ativar restaurante' )
-print('4. Sair\n')
-#snake_case
-
-print("1. Cadastrar Restaurante", "2. Listar Restaurante ","3. Ativar restaurante","4. Sair", sep='\n')
-#sep='\n' toda vez que ocorrer a separação, uma linha é pulada 
 print("""
 
 1. Cadastrar Restaurante
@@ -28,11 +15,15 @@ print("""
       
 """)
 
-# o mesmo resultado uando 3 aspas """ content """
+opcao_escolhida = int(input('Escolha uma opção: '))
 
-opcao_escolhida = input('Escolha uma opção: ')
+# escolha = int(opcao_escolhida)
 
-print(f'Você escolheu {opcao_escolhida}' )
-
-
-
+if opcao_escolhida == 1 :
+        print('Cadastrar Restaurante')
+elif opcao_escolhida == 2:
+        print("Listar restaurante")
+elif opcao_escolhida == 3:
+        print("Ativar restaurante")
+else:
+        print("Encerrar o Programa")
