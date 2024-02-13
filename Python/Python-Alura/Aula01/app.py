f365e35a84eec534d93b4794f4a10838f1431373
rafaelrvs <rafaelvieira1333@gmail.com>
2024-02-13 19:24:43 -0300

    aula 01

---
+++ Python/Python-Alura/Aula01/app.py
@@ -0,0 +1,15 @@
+print('Sabor express \n')
+
+
+
+print('1. Cadastrar Restaurante' )
+print('2. Listar Restaurante' )
+print('3. Ativar restaurante' )
+print('4. Sair\n')
+#snake_case
+opcao_escolhida = input('Escolha uma opção: ')
+
+print(f'Você escolheu {opcao_escolhida}' )
+
+
+
