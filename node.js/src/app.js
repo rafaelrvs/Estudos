f0602d2f053ab3046ddcf541be8f25d8e65b39ff
rafaelrvs <rafaelvieira1333@gmail.com>
2024-03-23 21:35:27 -0300

    adicionando put

--- node.js/src/app.js
+++ node.js/src/app.js
@@ -2,8 +2,61 @@ import  express  from "express";
 
 
 const app = express(); // importando a biblioteca e executando a função express para dentro da variavel app
+
+
+
+function buscaLivro(id){
+    return livros.findIndex( livros =>{
+        return livros.id === Number(id);
+    })
+}
+
+
+
+app.use(express.json())
+const livros = 
+[
+    {
+    id: 1,
+    autor:"chico"
+    },
+    {
+        id: 2,
+        autor:"japo"
+    }
+    
+
+
+
+
+];
+
 app.get("/", (req,res) =>{
     res.status(200).send("curso de node.js");
 });// passando  a responsabilidade para o express de gerenciar as rotas, ouvindo as  rotas sendo chamadas
+
+
+app.get("/livros", (req,res)=>{
+    res.status(200).json(livros);
+});
+
+
+app.get("/livros/:id",(req, res)=>{
+    const index = buscaLivro(req.params.id)
+    res.status(200).json(livros[index]);
+    
+});
+
+app.post("/livros", (req,res)=>{
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso");
+});
+
+app.put("/livros/:id", (req, res)=>{
+    const index = buscaLivro(req.params.id)
+    livros[index].autor = req.body.autor; //pegando uma propriedade e passando como parametro
+    res.status(200).json(livros)
+})
+
 
 export default app;
